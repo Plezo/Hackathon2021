7fad3f068c92e41508cfcf56909b48dbc73ac947
BuildTools <unconfigured@null.spigotmc.org>
2021-01-24 03:41:35 -0500

    Editted home page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React from 'react'
+import React from 'react';
 import './App.css';
-import { BrowserRouter } from 'react-router-dom'
-import Home from './pages'
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages';
+import SigninPage from './pages/signin';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Home />
-    </BrowserRouter>
+    <Router>
+      <Switch>
+        <Route path='/' component={Home} exact />
+        <Route path='/signin' component={SigninPage} exact />
+      </Switch>
+    </Router>
   );
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,30 @@
-import React, { useState } from 'react'
-import Sidebar from '../components/Sidebar'
-import Navbar from '../components/Navbar'
-import HomeSection from '../components/HomeSection'
+import React, { useState } from 'react';
+import Navbar from '../components/Navbar';
+import Sidebar from '../components/Sidebar';
+import Footer from '../components/Footer';
+import HeroSection from '../components/HeroSection';
+import InfoSection from '../components/InfoSection';
+import {
+  homeObjOne,
+} from '../components/InfoSection/Data';
+import Services from '../components/Services';
 
-const Home = () => {
+function Home() {
+  const [isOpen, setIsOpen] = useState(false);
 
-    const [isOpen, setIsOpen] = useState(false);
-
-    const toggle = () => {
-        setIsOpen(!isOpen);
-    }
-
-    return (
-        <>
-            <Sidebar isOpen={ isOpen } toggle={ toggle } />
-            <Navbar toggle={ toggle } />
-            <HomeSection />
-        </>   
-    )
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+  return (
+    <>
+      <Sidebar isOpen={isOpen} toggle={toggle} />
+      <Navbar toggle={toggle} />
+      <HeroSection />
+      <InfoSection {...homeObjOne} />
+      <Services />
+      <Footer />
+    </>
+  );
 }
 
-export default Home
+export default Home;
